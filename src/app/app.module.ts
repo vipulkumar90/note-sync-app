1c7d307ed994ac9ef72fb7f168371d9db31055b1
Vipul Kumar <vipulchaudhary64@gmail.com>
2023-08-06 13:07:01 +0530

    Added note create, note list, note delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddNoteComponent } from './add-note/add-note.component';
+import { NoteCardComponent } from './note-card/note-card.component';
+import { MainComponent } from './main/main.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { A11yModule } from '@angular/cdk/a11y';
+import { ClickedOutsideDirective } from './directives/clicked-outside.directive';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxMasonryModule } from 'ngx-masonry';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddNoteComponent,
+    NoteCardComponent,
+    MainComponent,
+    ClickedOutsideDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    A11yModule,
+    NgbModule,
+    NgxMasonryModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
